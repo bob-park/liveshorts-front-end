68bac70b3039caf72eb7e8e703542c8f2bfecc78
Bob Park <hwpark11@kakao.com>
2024-03-06 22:41:32 +0900

    feat: skeleton asset 추가

--- src/app/(default)/browse/page.tsx
+++ src/app/(default)/browse/page.tsx
@@ -1,11 +1,11 @@
+import SkeletonAsset from '@/app/components/asset/SkeletonAsset';
+
 export default function Browse() {
   return (
-    <div>
-      <video
-        src="/api/v1/shorts/task/2024-dab691b7-c765-4445-8828-3e5f2aa2aa48/resource"
-        controls
-        width={300}
-      />
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6 justify-items-center content-center">
+      {new Array(8).fill('').map((value, index) => (
+        <SkeletonAsset key={`skeleton-asset-${index}`} />
+      ))}
     </div>
   );
 }
