f8f8c872a32284a6e53c4c2369f321284c9de780
Bob Park <hwpark11@kakao.com>
2024-03-15 21:50:16 +0900

    feat: asset 정보 표시

--- src/app/(default)/asset/[assetId]/AssetPlayerContents.tsx
+++ src/app/(default)/asset/[assetId]/AssetPlayerContents.tsx
@@ -5,12 +5,36 @@ import AssetPlayer from '@/app/components/asset/AssetPlayer';
 import {} from 'react';
 
 // nextjs
-
 // react icons
 
-export default function AssetPlayeContents(props: { assetId: number }) {
+// timeago
+import TimeAgo from 'timeago-react';
+import * as timeago from 'timeago.js';
+import ko from 'timeago.js/lib/lang/ko';
+
+timeago.register('ko', ko);
+
+type AssetPlayeContentsProps = {
+  assetId: Number;
+  title: string;
+  description?: string;
+  createdDate: Date;
+  createdBy: string;
+  lastModifiedDate?: Date;
+  lastModifiedBy?: string;
+};
+
+export default function AssetPlayeContents(props: AssetPlayeContentsProps) {
   // props
-  const { assetId } = props;
+  const {
+    assetId,
+    title,
+    description,
+    createdDate,
+    createdBy,
+    lastModifiedDate,
+    lastModifiedBy,
+  } = props;
 
   return (
     <div className="grid grid-cols-1 gap-4">
@@ -18,6 +42,17 @@ export default function AssetPlayeContents(props: { assetId: number }) {
         <AssetPlayer
           src={`/api/v1/asset/${assetId}/resource?fileType=HI_RES&${Date.now()}`}
         />
+      </div>
+      <div className="col-span-1 mt-2">
+        <h2 className="text-xl font-bold">{title}</h2>
+      </div>
+      <div className="col-span-1">
+        <h4 className="text-md text-gray-500">
+          <TimeAgo datetime={createdDate} locale="ko" />
+        </h4>
+      </div>
+      <div className="col-span-1">
+        <h4>{description}</h4>
       </div>
     </div>
   );
--- src/app/(default)/asset/[assetId]/page.tsx
+++ src/app/(default)/asset/[assetId]/page.tsx
@@ -1,10 +1,44 @@
-// day
+// nextjs
+import { cookies } from 'next/headers';
 
 import AssetHeaderContents from './AssetHeaderContents';
 import AssetPlayeContents from './AssetPlayerContents';
 
-export default function AssetPage({ params }: { params: { assetId: number } }) {
+type Asset = {
+  assetId: number;
+  title: string;
+  description?: string;
+  assetStatus: string;
+  category: Category;
+  createdDate: Date;
+  createdBy: string;
+  lastModifiedDate?: Date;
+  lastModifiedBy?: string;
+};
+
+type Category = {
+  categoryId: number;
+  name: string;
+};
+
+// mam api host
+const MAM_API_HOST = process.env.MAM_API_HOST;
+
+export default async function AssetPage({
+  params,
+}: {
+  params: { assetId: number };
+}) {
   const { assetId } = params;
+
+  const response = await fetch(MAM_API_HOST + `/api/asset/${assetId}`, {
+    method: 'get',
+    headers: {
+      Authorization: `Bearer ${cookies().get('accessToken')?.value || ''}`,
+    },
+  });
+
+  const asset: Asset = await response.json().then((res) => res.result);
 
   return (
     <div className="grid grid-cols-1 gap-4 px-5 py-2">
@@ -15,7 +49,13 @@ export default function AssetPage({ params }: { params: { assetId: number } }) {
       <div className="col-span-1">
         <div className="grid grid-cols-2 xl:grid-cols-3 gap-10 justify-center items-center">
           <div className="col-span-2">
-            <AssetPlayeContents assetId={assetId} />
+            <AssetPlayeContents
+              assetId={assetId}
+              title={asset.title}
+              description={asset.description}
+              createdDate={asset.createdDate}
+              createdBy={asset.createdBy}
+            />
           </div>
           {/* shortfrom task list */}
           <div className="col-span-1">shortform task list</div>
