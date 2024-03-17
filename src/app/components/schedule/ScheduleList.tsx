49bf3fa54f579b7a0f242490ee1f2afd15fbdea6
Bob Park <hwpark11@kakao.com>
2024-03-17 23:16:19 +0900

    feat: 스케쥴 아이템 추가중

--- src/app/components/schedule/ScheduleList.tsx
+++ src/app/components/schedule/ScheduleList.tsx
@@ -2,6 +2,10 @@ type ScheduleListProps = {
   schedules: RecordSchedule[];
 };
 
+const ScheduleListItem = () => {
+  return <div>item</div>;
+};
+
 export default function ScheduleList(props: ScheduleListProps) {
   // props
   const { schedules } = props;
