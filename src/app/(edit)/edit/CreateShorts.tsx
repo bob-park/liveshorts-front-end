b2aa07febce5888d4d330f695cc62656a8f95296
Honey-Punch <wkdwlcjf1234@gmail.com>
2024-03-17 14:33:11 +0900

    프로그레스 바 계산식 추가

--- src/app/(edit)/edit/CreateShorts.tsx
+++ src/app/(edit)/edit/CreateShorts.tsx
@@ -1,13 +1,35 @@
+"use client";
+
+import { useState } from "react";
+
 export default function CreateShorts() {
-  return (
-    <div className="grid grid-rows-[1fr,300px] h-full">
-      <div className="grid grid-cols-[300px,1fr] border-b">
-        <div className="border-r">작업 패널</div>
-        <div>
-          <video src="/Users/cjc/Downloads/test.mp4"></video>
-        </div>
-      </div>
-      <div className="">프로그레스바</div>
-    </div>
-  );
+	const [currentTime, setCurrentTime] = useState(0);
+	const [videoDuration, setVideoDuration] = useState(0);
+
+	return (
+		<div className="grid grid-rows-[1fr,300px] h-full">
+			<div className="grid grid-cols-[300px,1fr] border-b">
+				<div className="border-r">작업 패널</div>
+				<div className="">
+					<video
+						controls
+						src="../../dummy/dummy.mp4"
+						onTimeUpdate={(e) => {
+							setCurrentTime(e.currentTarget.currentTime);
+						}}
+						onLoadedMetadata={(e) => {
+							setVideoDuration(e.currentTarget.duration);
+						}}
+						className="w-full"
+					></video>
+				</div>
+			</div>
+			<div className="relative">
+				<div
+					style={{ left: `${(currentTime / videoDuration) * 100}%` }}
+					className="w-1 h-full bg-neutral-500 absolute"
+				></div>
+			</div>
+		</div>
+	);
 }
