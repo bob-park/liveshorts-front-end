29c944e1122c68638f55fdab5e3facf21f05eb81
Honey-Punch <96092525+honey-punch@users.noreply.github.com>
2024-03-18 10:25:41 +0900

    리소스 적용

--- src/app/(edit)/edit/CreateShorts.tsx
+++ src/app/(edit)/edit/CreateShorts.tsx
@@ -2,34 +2,36 @@
 
 import { useState } from "react";
 
-export default function CreateShorts() {
-	const [currentTime, setCurrentTime] = useState(0);
-	const [videoDuration, setVideoDuration] = useState(0);
+const TEST_ASSET_ID = "20";
 
-	return (
-		<div className="grid grid-rows-[1fr,300px] h-full">
-			<div className="grid grid-cols-[300px,1fr] border-b">
-				<div className="border-r">작업 패널</div>
-				<div className="">
-					<video
-						controls
-						src="../../dummy/dummy.mp4"
-						onTimeUpdate={(e) => {
-							setCurrentTime(e.currentTarget.currentTime);
-						}}
-						onLoadedMetadata={(e) => {
-							setVideoDuration(e.currentTarget.duration);
-						}}
-						className="w-full"
-					></video>
-				</div>
-			</div>
-			<div className="relative">
-				<div
-					style={{ left: `${(currentTime / videoDuration) * 100}%` }}
-					className="w-1 h-full bg-neutral-500 absolute"
-				></div>
-			</div>
-		</div>
-	);
+export default function CreateShorts() {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [videoDuration, setVideoDuration] = useState(0);
+
+  return (
+    <div className="grid grid-rows-[1fr,300px] h-full">
+      <div className="grid grid-cols-[300px,1fr] border-b">
+        <div className="border-r">작업 패널</div>
+        <div className="">
+          <video
+            controls
+            src={`/api/v1/asset/${TEST_ASSET_ID}/resource?fileType=HI_RES&t=${new Date().getTime()}`}
+            onTimeUpdate={(e) => {
+              setCurrentTime(e.currentTarget.currentTime);
+            }}
+            onLoadedMetadata={(e) => {
+              setVideoDuration(e.currentTarget.duration);
+            }}
+            className="w-full"
+          ></video>
+        </div>
+      </div>
+      <div className="relative">
+        <div
+          style={{ left: `${(currentTime / videoDuration) * 100}%` }}
+          className="w-1 h-full bg-neutral-500 absolute"
+        ></div>
+      </div>
+    </div>
+  );
 }
