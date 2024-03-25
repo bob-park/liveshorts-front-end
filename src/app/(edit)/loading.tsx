3906f538e42b6e4ed22b68675a74d5846435347b
Honey-Punch <wkdwlcjf1234@gmail.com>
2024-03-25 20:53:39 +0900

    라우팅 변경

--- src/app/(edit)/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function GlobalLoading() {
-  return (
-    <div className="flex gap-x-2 items-center w-full h-full">
-      <span className="loading loading-infinity loading-lg" />
-      <h4 className="font-bold text-xl">loading...</h4>
-    </div>
-  );
-}
