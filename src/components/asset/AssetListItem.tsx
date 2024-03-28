0829844b763e053132ee26af123746b3da3a8d5a
Bob Park <hwpark11@kakao.com>
2024-03-28 20:53:40 +0900

    feat: asset list view 에서 숏폼 여부 및 youtube 업로드 여부 추가

--- src/components/asset/AssetListItem.tsx
+++ src/components/asset/AssetListItem.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
 
+// react icons
+import { SiYoutubeshorts, SiYoutube } from 'react-icons/si';
+
 import { convertFileSize } from '@/utils/common';
 
 import TimeAgo from 'timeago-react';
@@ -23,8 +26,8 @@ export default function AssetListItem(props: AssetListItemProps) {
   );
 
   return (
-    <div className="grid grid-cols-7 gap-4 mx-10 my-2 p-1 rounded-xl transition ease-in-out delay-150 hover:shadow-2xl hover:-translate-y-1 hover:scale-100 duration-300">
-      <div className="col-span-1 h-24 flex justify-center items-center ">
+    <div className="grid grid-cols-7 gap-4 mx-10 my-2 px-2 py-3 rounded-xl transition ease-in-out delay-150 hover:shadow-2xl hover:-translate-y-1 hover:scale-100 duration-300">
+      <div className="col-span-1 h-24 flex justify-center items-center">
         <Image
           className="w-auto h-full rounded-xl object-contain"
           src={assetImageSrc}
@@ -34,10 +37,25 @@ export default function AssetListItem(props: AssetListItemProps) {
           onError={() => setAssetImageSrc('/default_thumbnail.png')}
         />
       </div>
-      <div className="col-span-2 flex justify-start items-center mx-5">
+      <div className="col-span-2 flex justify-start items-center mx-5 relative">
         <div className="tooltip max-w-full text-start" data-tip={asset.title}>
           <p className="w-full truncate font-bold">{asset.title}</p>
         </div>
+        {asset.uploadSnsCount > 0 &&
+          asset.shortFormCount === asset.uploadSnsCount && (
+            <div className="absolute top-0 left-0">
+              <div className="tooltip" data-tip="업로드">
+                <SiYoutube className="w-5 h-5 text-red-600" />
+              </div>
+            </div>
+          )}
+        {asset.shortFormCount > 0 && (
+          <div className="absolute top-0 left-0">
+            <div className="tooltip" data-tip="숏폼">
+              <SiYoutubeshorts className="w-5 h-5 text-red-600" />
+            </div>
+          </div>
+        )}
       </div>
       <div className="col-span-1 flex justify-center items-center">
         {asset.category && (
