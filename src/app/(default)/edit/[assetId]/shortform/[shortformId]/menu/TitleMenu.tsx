1dc5467901783773ce752460c6e4b282cf8c1ad2
Honey-Punch <96092525+honey-punch@users.noreply.github.com>
2024-04-08 17:18:36 +0900

    스타일링 변경

--- src/app/(default)/edit/[assetId]/shortform/[shortformId]/menu/TitleMenu.tsx
+++ src/app/(default)/edit/[assetId]/shortform/[shortformId]/menu/TitleMenu.tsx
@@ -1,14 +1,12 @@
-import { TitleContent, WorkMenu } from '../type';
-import TitleItem from './TitleItem';
+import { TitleContent, WorkMenu } from "../type";
+import TitleItem from "./TitleItem";
 
 interface TitleMenuProps {
   titleContent: TitleContent | null;
   optionArray: string[];
   handleClickAddTitle: () => void;
   handleClickDeleteTitle: () => void;
-  handleChangeTitle: (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
-  ) => void;
+  handleChangeTitle: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
   handleClickWorkMenu(workMenu: WorkMenu): void;
 }
 
@@ -22,6 +20,11 @@ export default function TitleMenu({
 }: TitleMenuProps) {
   return (
     <div className="relative h-full p-2">
+      {!titleContent && (
+        <button disabled={!!titleContent} onClick={handleClickAddTitle} className="btn w-full">
+          + 제목 추가
+        </button>
+      )}
       {titleContent && (
         <TitleItem
           title={titleContent}
@@ -29,19 +32,9 @@ export default function TitleMenu({
           handleClickDeleteTitle={handleClickDeleteTitle}
           handleChangeTitle={handleChangeTitle}
           handleClickWorkMenu={() => {
-            handleClickWorkMenu('title');
+            handleClickWorkMenu("title");
           }}
         />
-      )}
-
-      {!titleContent && (
-        <button
-          disabled={!!titleContent}
-          onClick={handleClickAddTitle}
-          className="btn absolute bottom-4 right-4"
-        >
-          + 제목 추가
-        </button>
       )}
     </div>
   );
