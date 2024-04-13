998d3881c60b3df318f0c3c4ee077ef12abd63b8
Bob Park <hwpark11@kakao.com>
2024-04-13 12:47:55 +0900

    feat: tanstak query provider 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
 // store providers
-import Providers from "@/store/Providers";
+import Providers from '@/store/Providers';
 
-import "./globals.css";
+import './globals.css';
+import RQProvider from './_component/RQProvivder';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "LiveShorts",
-  description: "LiveShorts",
+  title: 'LiveShorts',
+  description: 'LiveShorts',
 };
 
 export default async function RootLayout({
@@ -21,11 +22,16 @@ export default async function RootLayout({
   return (
     <html lang="ko">
       <head>
-        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta
+          httpEquiv="Cache-Control"
+          content="no-cache, no-store, must-revalidate"
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <RQProvider>
+          <Providers>{children}</Providers>
+        </RQProvider>
       </body>
     </html>
   );
