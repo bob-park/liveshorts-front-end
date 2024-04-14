37ce66c33444d0160b7514b22183fceb7b9f7fdb
Bob Park <hwpark11@kakao.com>
2024-04-14 11:29:50 +0900

    refactor: loading 높이 수정

--- src/app/(default)/loading.tsx
+++ src/app/(default)/loading.tsx
@@ -1,6 +1,6 @@
 export default function GlobalLoading() {
   return (
-    <div className="flex gap-x-2 justify-center items-center w-screen h-screen">
+    <div className="flex gap-x-2 justify-center items-center w-screen h-[calc(100lvh-25rem)]">
       <span className="loading loading-infinity loading-lg" />
       <h4 className="font-bold text-xl">loading...</h4>
     </div>
